b91247f2c157953c688b0d65eb4f50a4d6c3f44c
Cristian Antonio Araya <dev.arayacristian@gmail.com>
2022-06-09 20:55:13 -0400

    [feat] Create a socket server
    -Create socket server using socket.io
    
    -Create a index.html file in public folder to create client
    and emit and receive message from the backend
    
    -Create a event call welcome-message who send data to each client
    when they connect from the first time

---
+++ README.md
@@ -0,0 +1,25 @@
+# **Server Using Socket.io and express**
+
+# *How to Run*
+
+Clone the repository and reinstall the node_modules using
+
+### **`npm install`**
+
+Then Run one of the following scripts in terminal,
+
+# *Available Scripts*
+
+### **`npm run start`**
+
+Runs the server in the production mode.
+
+Launch the server using `Node` in port `4000`.
+
+### **`npm run dev`**
+
+Runs the server in the development mode.
+
+Launch the server using `Nodemon` in port `4000`.
+
+[Nodemon](https://www.npmjs.com/package/nodemon) package is required to use this script.
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 
 //Express server
-const app = require('express')();
+const express = require('express')
+
+const app = express();
 
 //Socket server
 const server = require('http').createServer(app);
@@ -9,8 +11,16 @@ const server = require('http').createServer(app);
 //Socket server config
 const io = require('socket.io') ( server );
 
-io.on('connection', () => { })
+// Deploy public directory
+
+app.use( express.static(__dirname + '/public'))
+
+io.on('connection', ( socket ) => {
+    //Send message to client using emit
+    //welcome-message is the event name, and is requiered in the client to listen this event
+    socket.emit('welcome-message', { msg: 'Welcome to the Socket server!!', date: new Date()})
+ })
 
 server.listen( 4000 , () => {
-    console.log('Server running in server :4000')
+    console.log('Server running in server :4000');
 })
